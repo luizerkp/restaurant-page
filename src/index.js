b2b52e1792b0f8843ce0ca52faaf4069d05be39a
Luis Tamarez <luistamarez@hotmail.com>
2022-06-30 18:26:46 -0700

    Refactor footer content to a separate file     * This will provide code resusability     * Add scatfold header content

--- src/index.js
+++ src/index.js
@@ -1,38 +1,44 @@
 import './css/style.css';
 import './css/normalize.css';
-import githubLogo from './imgs/GitHubMarkSmall.png';
+import { footer } from './footerContent.js';
+// import logo from './imgs/.png';
 
-
-// adds footer content to the page
-var footer = (function() {
-    const footerDiv = document.createElement('div');
-    footerDiv.classList.add('footer');
-    const footerPara_1 = document.createElement('p');
-    const footerPara_2 = document.createElement('p');
-    const a = document.createElement('a');
-    a.href = "https://github.com/luizerkp";
-    const myGithubLogo = document.createElement('img');
-    myGithubLogo.src= githubLogo
-    a.appendChild(myGithubLogo)
-    a.setAttribute('id', 'github-logo');
-    const date = new Date().getFullYear();
-    footerPara_1.textContent = `Copyright © ${date} Luis Tamarez`
-    footerPara_2.textContent = "All Rights Reserved";
-    footerDiv.appendChild(footerPara_1);
-    footerDiv.appendChild(a);
-    footerDiv.appendChild(footerPara_2);
-
-    const buildFooter = () => {
-        return footerDiv;
-    }
-
-    return {
-        buildFooter: buildFooter
-    }
-    
-})();
 
 var buildContent = (function (){
-    // const content = document.querySelector('#content');
-    document.body.appendChild(footer.buildFooter());
+    const content = document.querySelector('#content');
+    footer.buildFooter();
+
+    return {
+        getContentDiv: () => content
+    }
+
+
+})();
+
+var header = (function () {
+    const rightP = document.createElement('p');
+    rightP.textContent = "This is the right div";
+
+    const leftP = document.createElement('p');
+    leftP.textContent = "This is the right div";
+
+    const headerDiv = document.createElement('header');
+    headerDiv.setAttribute('id', 'header');
+    const contentDiv = buildContent.getContentDiv();
+
+    const headerLeftDiv = document.createElement('div');
+    headerLeftDiv.setAttribute('id', 'header-left');
+    // const myLogo = document.createElement('img');
+    // myLogo.src = logo;
+    headerLeftDiv.appendChild(leftP);
+
+    const headerRightDiv = document.createElement('div');
+    headerRightDiv.setAttribute('id', 'header-right');
+    headerRightDiv.appendChild(rightP);
+
+    headerDiv.append(headerLeftDiv, headerRightDiv);
+
+
+
+    contentDiv.appendChild(headerDiv);
 })();
